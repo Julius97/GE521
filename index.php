91ffa47122624ff105e944fac76046b8f1c16822
Julius Rückin <julius.rueckin@rats-os.de>
2015-01-09 17:58:21 +0100

    chnaged viewport for mobile devices

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 		<script type="text/javascript" src="domscript.js"></script>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
 		<div id="webseite">
